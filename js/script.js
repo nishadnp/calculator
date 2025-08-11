ad400344ba0e5dc8940ace143bc9a358314667ba
nishadnp <69951135+nishadnp@users.noreply.github.com>
2025-08-11 16:03:26 +0530

    refactor: Add two seperate click button events
    Split single button functions into two independant ones:
    Make a click button event function for number buttons.
    Make another click button event function for operator buttons.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,37 @@
 // Calculator 
 
+const numberButtons = document.querySelectorAll('.numerical');
+const operatorButtons = document.querySelectorAll('.non-numerical.operators');
 
-const buttons = document.querySelectorAll('.numerical, .non-numerical');
 const firstDisplay = document.querySelector('.display.display-1');
 
 
 
-buttons.forEach(button => {
+// Button event for numbers
+numberButtons.forEach(button => {
     button.addEventListener('click', () => {
-        firstDisplay.textContent = firstDisplay.textContent + button.value;
+
+        const currentValue = button.value;
+        
+        
+        
+        firstDisplay.textContent = firstDisplay.textContent + currentValue;
+        
+    })
+
+});
+
+
+// Button event for operators
+operatorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const currentValue = button.value;
+
+        firstDisplay.textContent = firstDisplay.textContent + currentValue;
+
     })
 });
+
+
+
+
